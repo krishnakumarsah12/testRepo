8a70b8ff18c78bc028531146eccd761bd32e622f
Krishna kumar sah <krishna2053.kg@gmail.com>
2022-09-14 10:47:17 +0530

    first commit

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by krishna kumar sah on 13/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
